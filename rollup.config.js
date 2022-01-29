b6c0d44464b85ef7264d8cf5cd39694b29467b7b
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2022-01-29 13:00:21 +0700

    multi input

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import typescript from 'rollup-plugin-typescript2';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import { eslint } from 'rollup-plugin-eslint';
+import multiInput from 'rollup-plugin-multi-input';
 import pkg from './package.json';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
@@ -9,15 +10,16 @@ const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
 
 export default {
-    input: 'src/index.ts',
+    input: ['src/index.ts', 'src/modules/**/*.ts'],
     output: [
         {
-            file: pkg.main,
+            dir: 'dist',
             format: 'esm',
         },
     ],
     external,
     plugins: [
+        multiInput(),
         resolve({ extensions }),
         eslint(),
         typescript({
